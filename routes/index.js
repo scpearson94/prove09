8174fd0e480e9dafe30087caa0a6dc95f7b78e01
Sophia Pearson <scpearson94@byui.edu>
2020-07-12 21:44:25 -0700

    change root accedd in index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(__dirname + '../public/forms/form.html');
+  res.sendFile('/app/public/forms/form.html');
 });
 
 module.exports = router;
